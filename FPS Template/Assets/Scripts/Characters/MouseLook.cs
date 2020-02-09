53d783c6e746468d40eadfb9fab4d86f42a037ee
Deosarran Ramsingh <infamouskid156@live.com>
2020-02-08 20:02:16 -0500

    rewrites

---
+++ FPS Template/Assets/Scripts/Characters/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform PlayerBody;
+    public float mouseSensitivity = 100f;
+    public float AxisStop = 45f;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= MouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -AxisStop, AxisStop);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        PlayerBody.Rotate(Vector3.up * MouseX);
+    }
+}
