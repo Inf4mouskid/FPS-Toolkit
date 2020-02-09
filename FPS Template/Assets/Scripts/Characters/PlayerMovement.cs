efef3fa2821b9f6e6eff6367a6e7c4d816b59355
Deosarran Ramsingh <infamouskid156@live.com>
2020-02-09 00:35:05 -0500

    new player movement

--- FPS Template/Assets/Scripts/Characters/PlayerMovement.cs
+++ FPS Template/Assets/Scripts/Characters/PlayerMovement.cs
@@ -6,6 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController Controller;
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+
+    Vector3 velocity;
+    bool isGrounded;
+    int jumps;
 
     // Update is called once per frame
     void Update()
@@ -13,7 +23,22 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         Vector3 move = transform.right * x + transform.forward * z;
         Controller.Move(move * speed * Time.deltaTime);
+
+        if (isGrounded && Input.GetButtonDown("Jump") && jumps < 1)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        Controller.Move(velocity * Time.deltaTime);
     }
 }
